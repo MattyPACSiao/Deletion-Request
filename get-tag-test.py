348310fa184730f58a007b69b6fc5d36bbf77126
Quiba <mquiba001@chw.edu>
2024-01-02 12:13:46 -0800

    import url from ini

--- get-tag-test.py
+++ get-tag-test.py
@@ -1,12 +1,26 @@
-import sys
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+# import sys
+# import tkinter as tk
+# from tkinter import ttk
+# from tkinter import messagebox
 
-input = sys.argv[1:]
+# input = sys.argv[1:]
 
-root = tk.Tk()
-label = tk.Label(root, text=input, font=("Comic Sans MS", 27))
-label.pack(padx=20, pady=20)
+# root = tk.Tk()
+# label = tk.Label(root, text=input, font=("Comic Sans MS", 27))
+# label.pack(padx=20, pady=20)
 
-root.mainloop()
+# root.mainloop()
+
+import configparser
+
+# Create a ConfigParser object
+config = configparser.ConfigParser()
+
+# Read the INI file
+config.read('config.ini')
+
+# Access the variable from the specified section
+variable_value = config.get('URLs', 'FORMURL')
+
+# Now you can use the variable in your script
+print("variable_name:", variable_value)
